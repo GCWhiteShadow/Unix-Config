b98658fcceaf08af0351d6d2aa25ef790a0c3633
GCWhiteShadow <GCWhiteShadow@gmail.com>
2019-03-18 08:55:59 +0800

    add my config files

--- setup.sh
+++ setup.sh
@@ -1,14 +1,16 @@
 echo "***** Installing some necessary program, password required *****"
 sudo apt-get update
-sudo apt-get install tmux git vim
+sudo apt-get install tmux git vim --yes
 
 echo "Setting up tmux"
 cp -R Unix-Config/.tmux ~/
 ln -s -f .tmux/.tmux.conf
+cp Unix-Config/.tmux.conf.local ~/
 echo ""
 
 
 echo "Setting up Vim"
 cp -R Unix-Config/Vundle.vim ~/
+cp Unix-Config/.vimrc ~/
 echo "Please execute VundleInstall under vim"
 
