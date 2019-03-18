a85a89ee5c89cb849e38058124ab4c63c41965be
GCWhiteShadow <GCWhiteShadow@gmail.com>
2019-03-18 08:48:08 +0800

    renew & add submodules

---
+++ setup.sh
@@ -0,0 +1,13 @@
+echo "Installing some necessary program, password required"
+sudo apt-get install tmux git vim
+
+echo "Setting up tmux"
+cp -R Unix-Config/.tmux ~/
+ln -s -f .tmux/.tmux.conf
+echo ""
+
+
+echo "Setting up Vim"
+cp -R Unix-Config/Vundle.vim ~/
+echo "Please execute VundleInstall under vim"
+
